2890de022913b87a1b885bbcd63d3586bc3fbd52
Alberto <betim009@gmail.com>
2023-03-25 01:07:31 -0300

    req 1 - read csv

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,15 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
-#Iniciando projeto
+file = "data/jobs.csv"
+
+
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path) as file:
+        jobs_reader = list(csv.DictReader(file))
+        return jobs_reader
 
 
 def get_unique_job_types(path: str) -> List[str]:
