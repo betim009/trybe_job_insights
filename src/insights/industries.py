3cc6741e910b0f8cd7eee855c8728acfcb9893c8
Alberto <betim009@gmail.com>
2023-03-27 18:10:44 -0300

    req 7 - busca por industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 
 from src.insights.jobs import read
+
 # from jobs import read
 
 
@@ -20,18 +21,4 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [job for job in jobs if job["industry"] == industry]
